95670164de5570afa74e7e6718eac7086e3fb75b
Iliya-Yadegari <naruto3@email.com>
2021-06-06 08:06:08 +0100

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ===>')
+dateOfBirth = input('What is your date of birth? ===>')
+phone_num = input('What is your phone number? ===>')
+
+print(f'Your name is {name} your date of birth is {dateOfBirth} your phone number is {phone_num}')
