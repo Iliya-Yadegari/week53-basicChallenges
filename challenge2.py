fa6f435281b1cd7585025d100604c49158455e15
Iliya-Yadegari <naruto3@email.com>
2021-06-06 08:09:46 +0100

    Create challenge2.py

---
+++ challenge2.py
@@ -0,0 +1,6 @@
+num1 = input('Enter your first number ===> ')
+num2= input('Enter your two number ===> ')
+num3 = input('Enter your third number ===> ')
+
+print(f'Your numbers forward: \n {num1} \n {num2} \n {num3}')
+print(f'Your numbers reversed: \n {num3} \n {num2} \n {num1}')
