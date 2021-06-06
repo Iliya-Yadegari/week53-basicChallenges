272619513ab6e2376265999dca7ffcdd3af1bdf5
Iliya-Yadegari <naruto3@email.com>
2021-06-06 10:56:02 +0100

    Create challenge10.py

---
+++ challenge10.py
@@ -0,0 +1,9 @@
+num1 = int(input('Enter the first number ===> '))
+num2 = int(input('Enter the two number ===> '))
+num3 = int(input('Enter the third number ===> '))
+num4 = int(input('Enter the fourth number ===> '))
+
+total = num1 + num2 + num3 + num4
+average = total / 4
+
+print(f'The total is {total} and the average is {average}')
